09f1b995e0139d15e842975a3b94591ddbe7f489
venky348 <venkateshtalasila348@gmail.com>
2026-03-12 00:06:22 -0400

    Configure EF Core with SQLite and initialize database

--- src/TransactionIngestion/Program.cs
+++ src/TransactionIngestion/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TransactionIngestion.Data;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false)
+    .Build();
+
+var services = new ServiceCollection();
+
+services.AddLogging(config =>
+{
+    config.AddConsole();
+});
+
+services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(configuration["Database:ConnectionString"]);
+});
+
+var serviceProvider = services.BuildServiceProvider();
+
+using var scope = serviceProvider.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+db.Database.EnsureCreated();
+
+Console.WriteLine("Database initialized.");
